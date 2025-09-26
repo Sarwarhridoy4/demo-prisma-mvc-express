482f9bf1cb0af5dee3108871b186580c7331d8d7
Sarwar Hossain <sarwarhridoy4@gmail.com>
2025-09-26 21:06:28 +0600

    added nesessary script

--- prisma/migrations/20250926150354_initial_migration/migration.sql
+++ prisma/migrations/20250926150354_initial_migration/migration.sql
@@ -6,6 +6,7 @@ CREATE TABLE "public"."User" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "phone" TEXT,
     "password" TEXT NOT NULL,
     "role" "public"."Role" NOT NULL DEFAULT 'USER',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -16,3 +17,6 @@ CREATE TABLE "public"."User" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phone_key" ON "public"."User"("phone");
