ed4d492f35c673e3777784b57a559c18fc65fdc2
Sarwar Hossain <sarwarhridoy4@gmail.com>
2026-01-28 20:42:36 +0600

    update it for prisma 7.3 with multi file model eg: users, post.prisma and so on

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+import dotenv from "dotenv";
 
-export const prisma = new PrismaClient()
+dotenv.config();
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
